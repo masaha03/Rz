224715705eb6397a69be8a3a2fda00c9d0023048
Masahiro HAYASHI <mh@m884.jp>
2012-10-02 00:18:32 +0900

    "window" field

--- R/RzTools.R
+++ R/RzTools.R
@@ -1,17 +1,20 @@
 tools <-
 setRefClass("RzTools",
-  fields = c("variableView", "infoBar"),
+  fields = c("variableView", "infoBar", "window"),
   methods = list(
     initialize = function(...) {
       initFields(...)
       variableView <<- NULL
       infoBar      <<- NULL
+      window       <<- NULL
     },
     
     clean = function(){
       variableView <<- NULL
       infoBar      <<- NULL
+      window       <<- NULL
     }
   )
 )
-tools$accessors("variableView", "infoBar")
+tools$accessors("variableView", "infoBar", "window")
+
